7dc6645cc38621726043b2d8a9a5c67e514f1daa
LazyPottatto <rozi04@abv.bg>
2025-12-07 19:46:14 +0200

    Did the Home page and the styles for it

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,7 +6,8 @@ export default function Header() {
             <nav >
                 <div className="navbar">
                     <div className="nav-links"> 
-                        <Link className="logo" to="/"><img src="pictures/Pinpoint-Color.svg" alt="Logo" /></Link>
+                        <Link to="/"><img className="logo" src="pictures/Pinpoint-Color.svg" alt="Logo" /></Link>
+                        {/* <img className="heart" src="\pictures\heart-regular-full.svg" alt="heart" /> */}
                         <Link to="/likedPosts">Liked Posts</Link> {/*might just add a heart icon */}
                         <Link to="/followersPosts">Following</Link>
                     </div>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,18 +1,32 @@
 export default function Home() {
     return (
         <div>
-            {/* <section>
-                <img src="pictures/pexels-mikhaelmayim-30253591.jpg" alt="" />
-                <img src="pictures/pexels-shojol-30890859.jpg" alt="" />
-                <img src="pictures/pexels-laurachouette-21797582.jpg" alt="" />
-                <img src="pictures/pexels-tina-p-891488179-34136780.jpg" alt="" />
-                <img src="pictures/pexels-elijah-pilchard-269100825-19594515.jpg" alt="" />
-                <img src="pictures/pexels-zehra-guven-432246826-33039121.jpg" alt="" />
-                <img src="pictures/pexels-matteo-sarri-2152532088-32495056.jpg" alt="" />
-                <img src="pictures/pexels-olha-maltseva-2156976676-34483144.jpg" alt="" />
-                <img src="pictures/pexels-davide-negro-2157018402-34505016.jpg" alt="" />
-                <img src="pictures/pexels-lexi-lauwers-1431940-33225985.jpg" alt="" />
-            </section> */}
+            <div className="masonry">
+                <article className="card">
+                    <img src="pictures/pexels-mikhaelmayim-30253591.jpg" alt="" />
+                </article>
+                <article className="card"><img src="pictures/pexels-shojol-30890859.jpg" alt="" /></article>
+                <article className="card"><img src="pictures/pexels-tina-p-891488179-34136780.jpg" alt="" /></article>
+                <article className="card"><img src="pictures/pexels-laurachouette-21797582.jpg" alt="" /></article>
+                <article className="card"><img src="pictures/pexels-elijah-pilchard-269100825-19594515.jpg" alt="" /></article>
+                <article className="card"><img src="pictures/pexels-zehra-guven-432246826-33039121.jpg" alt="" /></article>
+                <article className="card"><img src="pictures/pexels-matteo-sarri-2152532088-32495056.jpg" alt="" /></article>
+                <article className="card"><img src="pictures/pexels-olha-maltseva-2156976676-34483144.jpg" alt="" /></article>
+                <article className="card"><img src="pictures/pexels-davide-negro-2157018402-34505016.jpg" alt="" /></article>
+                <article className="card"><img src="pictures/pexels-lexi-lauwers-1431940-33225985.jpg" alt="" /></article>
+                <article className="card">
+                    <img src="pictures/pexels-mikhaelmayim-30253591.jpg" alt="" />
+                </article>
+                <article className="card"><img src="pictures/pexels-shojol-30890859.jpg" alt="" /></article>
+                <article className="card"><img src="pictures/pexels-tina-p-891488179-34136780.jpg" alt="" /></article>
+                <article className="card"><img src="pictures/pexels-laurachouette-21797582.jpg" alt="" /></article>
+                <article className="card"><img src="pictures/pexels-elijah-pilchard-269100825-19594515.jpg" alt="" /></article>
+                <article className="card"><img src="pictures/pexels-zehra-guven-432246826-33039121.jpg" alt="" /></article>
+                <article className="card"><img src="pictures/pexels-matteo-sarri-2152532088-32495056.jpg" alt="" /></article>
+                <article className="card"><img src="pictures/pexels-olha-maltseva-2156976676-34483144.jpg" alt="" /></article>
+                <article className="card"><img src="pictures/pexels-davide-negro-2157018402-34505016.jpg" alt="" /></article>
+                <article className="card"><img src="pictures/pexels-lexi-lauwers-1431940-33225985.jpg" alt="" /></article>
+            </div>
         </div>
     );
 }
