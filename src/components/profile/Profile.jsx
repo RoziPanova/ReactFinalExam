7ef622c57ea22eb468362499b3ab1b7dfe5bea57
LazyPottatto <rozi04@abv.bg>
2025-12-10 01:12:39 +0200

    Did some styling on the profile page

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -3,15 +3,19 @@ export default function Profile() {
     const { userId } = useParams();
     return (
         <div className='profile-container'>
-            <img className='edit-user-icon' src="public\pictures\user-solid-full.svg" alt="icon" />
+            <img className='profile-user-icon' src="\pictures\user-solid-full.svg" alt="icon" />
             <div className="user-statistics">
                 <p><Link to={`/profile/${userId}/myPosts`}>My Posts</Link></p>
+                {/* //TODO: Replace "Count Following" and "Count Followers" with actual counts from the backend */}
                 <p className="followers-button">Count Following</p>
                 <p className="followers-button">Count Followers</p>
             </div>
-            <button className="edit-button">
+            <div className='button-container-edit-logout'>
+                <button className="edit-button">
                 <Link to={`/profile/${userId}/edit`}>Edit Profile</Link></button>
             <button className="logout-button">Log out</button>
+            </div>
+            
         </div>
     );
 }
