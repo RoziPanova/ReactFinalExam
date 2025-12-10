27c7aab3ab5c94b06ac1fb9cfc42365947ad6f15
LazyPottatto <rozi04@abv.bg>
2025-12-10 19:21:55 +0200

    Added spinner

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,20 +2,32 @@ import { useEffect, useState } from 'react';
 import CardPost from './card/CardPost';
 export default function Home() {
     const [posts, setPosts] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:3030/data/posts')
-            .then(res => res.json())
-            .then(result => {
-                setPosts(result);
-            })
+        setLoading(true);
+        try {
+            fetch('http://localhost:3030/data/posts')
+                .then(res => res.json())
+                .then(result => {
+                    setPosts(result);
+                })
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
     }, []);
     return (
         <div>
-            <div className="masonry">
-                {posts && posts.map(post => <CardPost key={post._id} {...post} />)}
-                {posts && posts.map(post => <CardPost key={post._id} {...post} />)}
-            </div>
+            {loading ? (
+                <div className="spinner"></div>
+            ) : (
+                <div className="masonry">
+                    {posts && posts.map(post => <CardPost key={post._id} {...post} />)}
+                    {posts && posts.map(post => <CardPost key={post._id} {...post} />)}
+                </div>
+            )}
         </div>
     );
 }
